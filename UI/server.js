1b000ee14b93a75c75a007e07652cc1e8def6d73
Finnamon1 <47517513+Finnamon1@users.noreply.github.com>
2025-09-30 16:41:21 +1300

    Update server.js

--- UI/server.js
+++ UI/server.js
@@ -4,7 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from UI/
+app.use(express.static(__dirname));
+
+// Explicitly handle root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
